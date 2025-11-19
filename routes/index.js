a3b30ab86ba720fe8ff2e2af5c92ab3a16466747
Bruno Teixeira <166347569+TXzzera@users.noreply.github.com>
2025-11-19 17:31:15 -0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
-    //#swagger.ignore = true
+    //#swagger.tags = ['Hello World']
     res.send('Hello World!');
 });
 
 router.use('/users', require('./users'));
-router.use('/cars', require('./cars'));
 
 module.exports = router;
 
--- routes/users.js
+++ routes/users.js
@@ -2,22 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users');
-const validateUser = require('../middlewares/userValidate');
+const validateUser = require('../middlewares/validate');
 
-router.get('/', 
-    //#swagger.tags = ['Users']
-    usersController.getAll);
-router.get('/:id',
-    //#swagger.tags = ['Users']
-    usersController.getSingle);
-router.post('/', 
-    //#swagger.tags = ['Users']
-    validateUser.saveUser, usersController.userCreate);
-router.put('/:id', 
-    //#swagger.tags = ['Users']
-    validateUser.saveUser, usersController.userUpdate);
-router.delete('/:id', 
-    //#swagger.tags = ['Users']
-    usersController.userDelete);
+router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingle);
+router.post('/', validateUser.saveUser, usersController.userCreate);
+router.put('/:id', validateUser.saveUser, usersController.userUpdate);
+router.delete('/:id', usersController.userDelete);
 
 module.exports = router;
